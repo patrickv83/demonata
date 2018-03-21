83eddfb3708da2cb2ee7c25e1b6e0d08404ac146
Patrick Vinas <pvinas@novetta.com>
2018-03-21 12:42:42 -0500

    Put coords back into room class, add EmptyRoom tests

--- demonata/room.py
+++ demonata/room.py
@@ -5,13 +5,40 @@ class Room():
     """This is the Room abstract class"""
 
     @abstractmethod
-    def __init__(self, description):
+    def __init__(self, x, y, description):
+        self.x = x
+        self.y = y
         self.description = description
+
+    @abstractmethod
+    def getCoordinates(self):
+        return x,y
 
     @abstractmethod
     def generateDescription(self):
         self.description = "Nothing to see here"
 
     @abstractmethod
-    def printDescription(self):
+    def getDescription(self):
         print self.description
+
+
+class EmptyRoom(Room):
+    """This is the EmptyRoom class"""
+
+    def __init__(self, x, y, description):
+        self.x = x
+        self.y = y
+        self.description = description
+
+    def getCoordinates(self):
+        return self.x, self.y
+
+    def generateDescription(self):
+        """TODO: Generate description based on contents and location of room"""
+        self.description = "Random placeholder room description"
+
+    def getDescription(self):
+        return self.description
+
+
--- tests/test_room.py
+++ tests/test_room.py
@@ -1,7 +1,14 @@
 import pytest
-from demonata.room import Room
+from demonata.room import Room, EmptyRoom
 
 def test_abstract_room_instantiate_fails():
     with pytest.raises(TypeError):
-        Room()
+        Room(1, 1, "")
 
+
+def test_empty_room_class_and_methods():
+    er = EmptyRoom(1, 1, "")
+    x, y = er.getCoordinates()
+    er.generateDescription()
+    desc = er.description
+    assert (x == 1 and y == 1 and desc == "Random placeholder room description")
