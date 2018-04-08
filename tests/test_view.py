3a4251f2d2500666780e1a2b095e1a063777cd33
Patrick Vinas <patrick.vinas@gmail.com>
2018-04-08 00:35:58 -0500

    Add view tests, update module names in tests

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,14 +1,16 @@
 import pytest
-from view import mainView
+from demonata.view.main_view import GameView
 
 def test_main_view():
-    descriptionText = 'main game text'
-    mapText = 'map'
+    description_text = 'main game text'
+    map_text = 'map'
     choice1 = 'choice1'
     choice2 = 'choice2'
     choice3 = 'choice3'
-    view = mainView.gameView(descriptionText, mapText, 
+    view = gameView(description_text, map_text, 
             [choice1, choice2, choice3])
-    assert view.screen.body[0].get_text()[0] == descriptionText
-    assert view.screen.body[1].get_text()[1] == mapText
-    assert view.screen.footer.
+    assert view.build().body[0].get_text()[0] == description_text
+    assert view.build().body[1].get_text()[1] == map_text
+    assert view.build().footer.original_widget.body[0][0]._w.original_widget.text == choice1
+    assert view.build().footer.original_widget.body[0][1]._w.original_widget.text == choice2
+    assert view.build().footer.original_widget.body[0][2]._w.original_widget.text == choice3
