f1ae82ac110a7a27c43bf700b1dae86243d6c7a0
Jennifer Shelby <jalyxis55@gmail.com>
2018-04-12 18:21:28 -0500

    Room Updates
    Room super class is complete
    EmptyRoom is complete
    Enemy and Artifact rooms will need their modifyPlayer funtions updated
    in the future

--- src/artifact_room.py
+++ src/artifact_room.py
@@ -5,15 +5,17 @@ from src.room import Room
 
 class ArtifactRoom(Room):
     def __init__(self, x, y, item):
-        """@ReturnType Artifact Room"""
-        self.___item = item
-        """@AttributeType Item"""
+        self.item = item
         super(ArtifactRoom, self).__init__(x, y)
 
-    def addArtifact(self):
-        """@ReturnType void"""
-        pass
+	def introText(self):
+		# print room description
+		return
 
     def modifyPlayer(self, player):
-        pass
+		# update player inventory with item found in room 
+        return player
+		
+	def getCoordinates(self):
+		return(self.x, self.y)
 
--- src/empty_room.py
+++ src/empty_room.py
@@ -3,13 +3,19 @@
 from src.room import Room
 
 class EmptyRoom(Room):
-    def introText(self):
-        """@ReturnType void"""
-        pass
+	# EmptyRoom constructor
+    def __init__(self, x, y):
+        super().__init__(x, y)
+	
+	# introText displays the room description
+	def introText(self):
+		return
+		
+	# modifyPlayer
+	# Empty room has no player modification
+	def modifyPlayer(self, player):
+		pass
 
-    def __init__(self, x, y, text):
-        self.description = text
-        super(EmptyRoom, self).__init__(x, y)
-
+	# Postcondition: Returns room's coordinates in the form of a tuple
     def getCoordinates(self):
-        return self.x, self.y
+        return (self.x, self.y)
