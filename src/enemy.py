d706d769f49eb90583f2c03e25ac818724199cb7
Jennifer Shelby <jalyxis55@gmail.com>
2018-04-17 22:54:33 -0500

    Updated test_enemy.py
    Updated test_enemy.py after changes to enemy.py

--- src/enemy.py
+++ src/enemy.py
@@ -24,29 +24,29 @@ class Enemy(Character):
 # and low armor
 class Brownie(Enemy):
     def __init__(self):
-        super(Brownie, self).__init__(name="Brownie", hp=10, dmg=2, enemyType=1)
+        super(Brownie, self).__init__(name="Brownie", hp=10, dmg=2, enemyType=BROWNIE)
 
 # Drow is a dark elf with magic and bludgeoning attacks. They have low hit
 # points and high armor
 class Drow(Enemy):
     def __init__(self):
-        super(Drow, self).__init__(name="Drow", hp=35, dmg=2, enemyType=2)
+        super(Drow, self).__init__(name="Drow", hp=35, dmg=2, enemyType=DROW)
 
 # Goblin has knife/sword attacks. The have medium hit points
 # and low armor
 class Goblin(Enemy):
     def __init__(self):
-        super(Goblin, self).__init__(name="Goblin", hp=20, dmg=15, enemyType=3)
+        super(Goblin, self).__init__(name="Goblin", hp=20, dmg=15, enemyType=GOBLIN)
 
 # Thug has bludgeoning or knife/sword attacks. They have medium hit points
 # and medium armor
 class Thug(Enemy):
     def __init__(self):
-        super(Thug, self).__init__(name="Thug", hp=25, dmg=15, enemyType=4)
+        super(Thug, self).__init__(name="Thug", hp=25, dmg=15, enemyType=THUG)
 
 # Demon is the boss enemy. Demons have magic and knife/sword attacks. They
 # have a special life drain attack. Demons have high hit points and high armor
 class Demon(Enemy):
     def __init__(self):
-        super(Demon, self).__init__(name="Demon", hp=75, dmg=20, enemyType=5)
+        super(Demon, self).__init__(name="Demon", hp=75, dmg=20, enemyType=DEMON)
         
--- tests/test_enemy.py
+++ tests/test_enemy.py
@@ -1,29 +1,33 @@
 import unittest
 from src.enemy import Enemy
-from src.weapon import Weapon
+#from src.weapon import Weapon
 
-e = Enemy("mean joe", 15)
-f = Enemy("sword gumby", 25, 3, weapon = Weapon("sword", "neat shiny sword", 3, 5))
+e = Brownie()
+#d = Drow()
+#g = Goblin()
+#t = Thug()
+#boss = Demon()
+#f = Enemy("Generic", 25, 3, GOBLIN)
 
 def test_enemy_instantiate():
-    assert (e.getName() == "mean joe" and e.getHP() == 15 and e.getWeapon().getName() == 'Cudgel')
+    assert (e.getName() == "Brownie" and e.getHP() == 10)
 
 def test_enemy_damage():
     e.takeDamage(5)
-    assert e.getHP() == 10
+    assert e.getHP() == 5
 
 def test_enemy_heal():
     e.heal(3)
-    assert e.getHP() == 13
+    assert e.getHP() == 8
 
 def test_enemy_name_change():
-    e.setName("dead mean joe")
-    assert e.getName() == "dead mean joe"
+    e.setName("dead Brownie")
+    assert e.getName() == "dead Brownie"
 
-def test_enemy_attack():
-    damage = e.attack(f)
-    assert f.getHP() == (25 - damage)
+#def test_enemy_attack():
+#    damage = e.attack(f)
+#    assert f.getHP() == (25 - damage)
 
 def test_enemy_dead():
-    e.takeDamage(13)
+    e.takeDamage(8)
     assert e.isDead() == True
