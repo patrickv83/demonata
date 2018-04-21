4c9735ef0ef0414ccdfb823957639d7da180c018
Lais DeLeau <l_d91@txstate.edu>
2018-04-20 22:21:32 -0500

    Make sytax fixes

--- src/consumable.py
+++ src/consumable.py
@@ -1,7 +1,11 @@
+""" Consumable Module
+    Defines the Consumable superclass
+    Defines each Consumable subclass
+    Defines an enumerated list of Enemy Types (CT) """
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-from src.item import Item
 from enum import Enum
+from src.item import Item
 
 # Enumeration for Consumables
 class CT(Enum):
@@ -11,54 +15,73 @@ class CT(Enum):
     APPLE = 3
     SNICKERS = 4
     HONEY_GLAZED_HAM = 5
-    
+
 class Consumable(Item):
     """ Consumable superclass creates a consumable """
     def __init__(self, name, desc, value, uses, consumableType):
         """@ReturnType Consumable"""
-        self.___uses = uses
-        self.___name = name
-        self.___consumableType = consumableType
+        self.uses = uses
+        self.name = name
+        self.consumableType = consumableType
         """@AttributeType int"""
         super(Consumable, self).__init__(name, desc, value)
 
     def getUses(self):
-        return self.___uses
+        """ Get the number of times the consumable can be used. """
+        return self.uses
 
     def getName(self):
-        return self.___name
-    
+        """ Get the name of the consumable. """
+        return self.name
+
     def consume(self):
-        self.___uses = self.getUses() - 1
-        
+        """ When the consumable is consumed, this method must be called.
+        This will decrease the number of uses the consumable has. """
+        self.uses = self.getUses() - 1
+
     def getValue(self):
-        if(self.getUses() == 0):
-            return 0;
+        """ Get the value of the consumable.  If the consumable has
+        no uses left, this will return 0. """
+        if self.getUses() == 0:
+            return 0
         return self.getValue()
 
     def __str__(self):
         """@ReturnType String"""
-        return "Name: {0}\nUses: {1}\nValue: {2}".format(self.___name, self.___uses, self.___value)
+        return "Name: {0}\nUses: {1}\nValue: {2}".format(self.name, self.uses, self._value)
 
-# Moldy Bread causes damage
 class MoldyBread(Consumable):
+    """Moldy bread causes damage"""
     def __init__(self):
-        super(MoldyBread, self).__init__(name="Moldy Bread", desc="A disgusting piece of moldy bread." value=-5, uses=2, consumableType=1)
-        
-# Bread heals a small amount
+        super(MoldyBread, self).__init__(name="Moldy Bread",
+                                         desc="A disgusting piece of moldy bread.",
+                                         value=-5, uses=2, consumableType=CT.MOLDY_BREAD)
+
 class Bread(Consumable):
+    """Bread heals a small amount"""
     def __init__(self):
-        super(Bread, self).__init__(name="Bread", desc="A square piece of white bread." value=5, uses=2, consumableType=2)
+        super(Bread, self).__init__(name="Bread",
+                                    desc="A square piece of white bread.",
+                                    value=5, uses=2, consumableType=CT.BREAD)
 
 # Yummy Apples
 class Apple(Consumable):
+    """Apples heal slightly more than bread"""
     def __init__(self):
-        super(Apple, self).__init__(name="Apple", desc="A pink lady apple of exquisite flavor." value=25, uses=5, consumableType=3)
-        
+        super(Apple, self).__init__(name="Apple",
+                                    desc="A pink lady apple of exquisite flavor.",
+                                    value=25, uses=5, consumableType=CT.APPLE)
+
 class Snickers(Consumable):
+    """Snickers satisfies you"""
     def __init__(self):
-        super(Snickers, self).__init__(name="Snickers", desc="You're not yourself when you're hungry." value=75, uses=3, consumableType=4)
-        
+        super(Snickers, self).__init__(name="Snickers",
+                                       desc="You're not yourself when you're hungry.",
+                                       value=75, uses=3, consumableType=CT.SNICKERS)
+
 class HoneyGlazedHam(Consumable):
+    """A whole ham will last a long time"""
     def __init__(self):
-        super(HoneyGlazedHam, self).__init__(name="Honey Glazed Ham", desc="A large spiral cut honey glazed ham." value=150, uses=25, consumableType=5)
+        super(HoneyGlazedHam, self).__init__(name="Honey Glazed Ham",
+                                             desc="A large spiral cut honey glazed ham.",
+                                             value=150, uses=25, consumableType=CT.HONEY_GLAZED_HAM)
