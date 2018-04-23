32da6da8ebf89d15513a44245aa3c0d85d22e541
DKlesel <dck35@txstate.edu>
2018-04-23 00:47:20 -0500

    Weapon class updates
    Added methods to generate random weapons. Call getWeapon() to get a
    weapon with a random name and damage.
    Things I need help with on this:
    1. I’m not sure what the call the super is for in the original code in
    the __init__ method.
    2. Original method only has one parameter for damage, so I’m not sure
    if I need to add a min and max damage or just leave it with just one
    value for damage.
    3. My interpreter is having issue with running the code because of
    coveralls so I am unable to test it out yet.
    4. Still need to implement tests which I will add later.

--- src/weapon.py
+++ src/weapon.py
@@ -4,19 +4,62 @@ from src.item import Item
 import random
 
 class Weapon(Item):
-    def __init__(self, name, desc, value, damage):
+    def __init__(self, damage, name, desc = " ", value = 0):
         """@ReturnType Weapon"""
-        self.___damage = damage
-        self.___name = name
+        self.__damage = damage
         """@AttributeType int"""
+        self.__name = name
+        """@AttributeType String"""
+        self.__value = value
+        """@AttributeType int"""
+        self.__desc = desc
+        """@AttributeTupe String"""
         super(Weapon, self).__init__(name, desc, value)
 
     def getDamage(self):
-        return self.___damage
+        return self.__damage
 
     def getName(self):
-        return self.___name
+        return self.__name
 
     def __str__(self):
         """@ReturnType String"""
-        return "Name: {0}\nDamage: {1}\nValue: {2}".format(self.___name, self.___damage, self.___value)
+        return "Name: {0}\nDamage: {1}\nValue: {2}".format(self.__name, self.__damage, self.__value)
+
+    @classmethod
+    def getWeapon(cls):
+        """@ReturnType Weapon"""
+        weaponDamage = random.randint(1,5)
+        weaponName = Weapon.__generateWeaponName()
+        weaponValue = weaponDamage
+        return cls(weaponDamage, weaponName, " ", weaponValue)
+
+    def __generateWeaponName(self):
+        """@ReturnType String"""
+        return Weapon.__getAdjective() + Weapon.__getNoun()
+
+    def __getAdjective(self):
+        """@ReturnType String"""
+        adjectives = ["Strong", "Weak", "Heavy", "Light", "Sharp", "Dull", "Blinding", "Cold", "Ice", "Fire",
+                               "Earth", "Wind", "Freezing", "Burning", "Frightening", "Scary", "Beautiful", "Splendid",
+                               "Atomic", "Basic", "Deadly", "Destructive", "Fatal", "Lethal", "Common", "Rare", "Epic",
+                               "Legendary", "Devastating", "Effective", "Great", "Awesome", "Indestructable", "New",
+                               "Invincible", "Ordinary", "Reliable", "Unreliable", "Sacred", "Simple", "Blessed",
+                               "Cursed", "Magical", "Superior", "Useless", "Usefull", "Valuable", "Invaluable", "Used",
+                               "Worthless", "Worthy", "Dreadful", "Enchanted", "Murderous", "Venomous", "Reliable",
+                               "Poisonous", "Ultimate", "Unique", "Savage", "Dreadful", "Powerful", "Broken",
+                               "Heavily Used", "Gently Used", "Pristine"]
+        # 66 Total Adjectives
+        return random.choice(adjectives)
+
+    def __getNoun(self):
+        """@ReturnType String"""
+        nouns = ["Sword", "Axe", "Knife", "Dagger", "Diabo", "Pickaxe", "Shiv", "Sickle", "Mattock",
+                          "Broadsword", "Katana", "Longsword", "Sabre", "Ulfberht", "Battle Axe", "Flail", "Mace",
+                          "Morning Star", "Spear", "Halberd", "Lance", "Bardiche", "Glaive", "Tomahawk", "Pike",
+                          "Scythe", "Bow", "Sling", "Long Bow", "Compound Bow", "Recurve Bow", "Throwing Axe",
+                          "Mongol Bow", "Crossbow", "Javelin", "Wand", "Staff", "Quarterstaff", "Club", "Bat", "Diabo",
+                          "Hammer", "Sledgehammer", "Maul", "Crowbar"]
+        # 45 Total Nouns
+        return random.choice(nouns)
+
