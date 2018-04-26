b7ddc432f5365e3e1ceecd04ec8fe737e5171eb3
DKlesel <dck35@txstate.edu>
2018-04-26 02:12:34 -0500

    fixing merge conflics

--- tests/test_player.py
+++ tests/test_player.py
@@ -5,13 +5,8 @@ from src.weapon import Weapon
 from src.consumable import *
 
 e = Enemy("Brownie", 10, 2, ET.BROWNIE)
-<<<<<<< Updated upstream
-p = Player("sword gumby", 25, weapon = Weapon(name="sword", damage=5))
-cons= Consumable.create();
-=======
 p = Player("sword gumby", 25, 3, weapon = Weapon(5, "neat shiny sword"))
 c = Consumable.create();
->>>>>>> Stashed changes
 
 def test_player_instantiate():
     assert (p.getName() == "sword gumby" and p.getHP() == 25)
@@ -33,13 +28,8 @@ def test_player_name_change():
 #    damage = p.attack(e)
 #    assert e.getHP() == (15 - damage)
 def test_player_consumable_heals():
-<<<<<<< Updated upstream
-    p.consume(cons)
-    assert p.getHP() == 23+cons.getValue()
-=======
     p.consume(c)
     assert p.getHP() == 23+c.getValue()
->>>>>>> Stashed changes
 
 def test_player_dead():
     p.takeDamage(50)
